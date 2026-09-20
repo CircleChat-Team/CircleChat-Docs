8e0a9a5e4ff99831155749c69c28e497775f16b7
EndlessPixel <system-mini@outlook.com>
2026-09-20 19:20:13 +0800

    Add files via upload

---
+++ theme/index.ts
@@ -0,0 +1,14 @@
+import type { App } from 'vue'
+import DefaultTheme from 'vitepress/theme'
+import WordCount from './WordCount.vue'
+import Layout from './Layout.vue'
+
+export default {
+  extends: DefaultTheme,
+  ...DefaultTheme,
+  Layout,
+
+  enhanceApp({ app }: { app: App }) {
+    app.component('WordCount', WordCount)
+  }
+}
